48943e94404ca29f454b5ed280da50c529223522
hyun-k86 <newtype86@naver.com>
2025-03-11 13:49:10 +0900

    첫번째 커밋

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './component/Header';
+import DayList from './component/DayList';
+import EmptyPage from './component/EmptyPage';
+import Day from './component/Day';
+import CreateWord from './component/CreateWord';
+import CreateDay from './component/CreateDay';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<DayList/>} />
+          <Route path='/day/:day' element={<Day/>} />
+          <Route path='/create_word' element={<CreateWord/>} />
+          <Route path='/create_day' element={<CreateDay/>} />
+          <Route path='*' element={<EmptyPage/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
